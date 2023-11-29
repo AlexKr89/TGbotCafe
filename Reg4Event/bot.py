42f77b742340b8dc5fd0654992e6f0f30f0fab66
Alex <kaf-2012@mail.ru>
2023-11-30 00:33:02 +0300

    modified bot.py

--- Reg4Event/bot.py
+++ Reg4Event/bot.py
@@ -1,13 +1,12 @@
+# bot.py
 import logging
-import sqlite3
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-import pandas as pd
-from sqlalchemy import create_engine
+from telegram.ext import Updater, CommandHandler, CallbackContext
 from config import TOKEN
+from database import events_df, subscribe, export_csv, start
 
 # Настройки бота
-DATABASE_URI = 'sqlite:///subscriptions.db'  # URI для SQLite, можно изменить на другую базу данных
+DATABASE_URI = 'sqlite:///subscriptions.db'
 
 # Настройки логгирования
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -15,46 +14,6 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 # Инициализация бота и базы данных
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
-engine = create_engine(DATABASE_URI, echo=True)
-conn = engine.connect()
-
-# Создание таблицы подписок, если она не существует
-conn.execute('''
-CREATE TABLE IF NOT EXISTS subscriptions (
-    chat_id INTEGER,
-    event_name TEXT,
-    PRIMARY KEY (chat_id, event_name)
-);
-''')
-
-# Загрузка мероприятий из XLS файла
-events_df = pd.read_excel('events.xlsx')
-
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Привет! Этот бот предоставляет информацию о мероприятиях. '
-                              'Используйте /events, чтобы увидеть доступные мероприятия.')
-
-def events(update: Update, context: CallbackContext) -> None:
-    event_list = "\n".join(events_df['event_name'])
-    update.message.reply_text(f"Доступные мероприятия:\n{event_list}")
-
-def subscribe(update: Update, context: CallbackContext) -> None:
-    chat_id = update.message.chat_id
-    event_name = context.args[0]
-
-    # Проверка наличия мероприятия
-    if event_name not in events_df['event_name'].values:
-        update.message.reply_text(f"Мероприятия {event_name} не существует.")
-        return
-
-    # Подписка пользователя
-    conn.execute('INSERT OR IGNORE INTO subscriptions (chat_id, event_name) VALUES (?, ?)', (chat_id, event_name))
-    update.message.reply_text(f"Вы подписались на мероприятие {event_name}.")
-
-def export_csv(update: Update, context: CallbackContext) -> None:
-    df = pd.read_sql('SELECT * FROM subscriptions', engine)
-    df.to_csv('subscriptions.csv', index=False)
-    update.message.reply_text("Экспорт подписок в CSV завершен.")
 
 # Обработка команд
 start_handler = CommandHandler('start', start)
