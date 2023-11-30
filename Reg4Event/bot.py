0ce674074e7f4b311443d97b1f5d334ee5e0fb62
Alex <kaf-2012@mail.ru>
2023-11-30 21:39:54 +0300

    modified bot.py

--- Reg4Event/bot.py
+++ Reg4Event/bot.py
@@ -1,5 +1,5 @@
 import logging
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from config import TOKEN
 from database import events_df, subscribe, export_csv, start
@@ -19,8 +19,21 @@ start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
 def events(update: Update, context: CallbackContext) -> None:
-    event_list = "\n".join(events_df['event_name'])
-    update.message.reply_text(f"Доступные мероприятия:\n{event_list}")
+    keyboard = []
+
+    for index, row in events_df.iterrows():
+        event_name = row['event_name']
+        event_date = row['event_date']
+
+        # Отображение event_date и кнопки "Записаться"
+        text = f"{event_name}\nДата: {event_date}"
+        button = InlineKeyboardButton("Записаться", callback_data=f"subscribe_{index}")
+        keyboard.append([text, button])
+
+    # Создание клавиатуры с кнопками
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text("Доступные мероприятия:", reply_markup=reply_markup)
 
 events_handler = CommandHandler('events', events)
 dispatcher.add_handler(events_handler)
