18b2ef841ec2d2b8c089774c1bcdd197050dddac
Alex <kaf-2012@mail.ru>
2023-11-30 22:51:50 +0300

    modified bot.py

--- Reg4Event/bot.py
+++ Reg4Event/bot.py
@@ -3,26 +3,59 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from config import TOKEN
 from database import events_df, subscribe, export_csv, start
-import pandas as pd
 
-# Bot and database setup
+# Настройки бота
 DATABASE_URI = 'sqlite:///subscriptions.db'
+
+# Настройки логгирования
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+# Инициализация бота и базы данных
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
-# Command handlers
+# Обработка команд
 start_handler = CommandHandler('start', start)
-events_handler = CommandHandler('events', events)
-subscribe_handler = CommandHandler('subscribe', subscribe, pass_args=True)
-export_csv_handler = CommandHandler('export_csv', export_csv)
-
-# Adding handlers to the dispatcher
 dispatcher.add_handler(start_handler)
+
+def events(update: Update, context: CallbackContext) -> None:
+    keyboard = []
+
+    # Проверка наличия столбца 'event_date'
+    if 'event_date' not in events_df.columns:
+        update.message.reply_text("Столбец 'event_date' не существует в DataFrame.")
+        return
+
+    for index, row in events_df.iterrows():
+        event_name = row['event_name']
+        event_date = row['event_date']
+
+        # Отображение event_date и кнопки "Записаться"
+        text = f"{event_name}\nДата: {event_date}"
+        button = InlineKeyboardButton("Записаться", callback_data=f"subscribe_{index}")
+        keyboard.append([button])
+
+    # Создание клавиатуры с кнопками
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    # Улучшенный вывод информации о мероприятиях
+    if not events_df.empty:
+        update.message.reply_text("Доступные мероприятия:")
+        for index, row in events_df.iterrows():
+            event_info = f"{row['event_name']} ({row['event_date']})"
+            update.message.reply_text(event_info, reply_markup=reply_markup)
+    else:
+        update.message.reply_text("Нет доступных мероприятий.")
+
+events_handler = CommandHandler('events', events)
 dispatcher.add_handler(events_handler)
+
+subscribe_handler = CommandHandler('subscribe', subscribe, pass_args=True)
 dispatcher.add_handler(subscribe_handler)
+
+export_csv_handler = CommandHandler('export_csv', export_csv)
 dispatcher.add_handler(export_csv_handler)
 
-# Start the bot
+# Запуск бота
 updater.start_polling()
 updater.idle()
