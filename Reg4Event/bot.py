03c1b2707d4711184bd21c2cac9a6201dca5e7b7
Alex <kaf-2012@mail.ru>
2023-11-30 23:23:06 +0300

    modified bot.py

--- Reg4Event/bot.py
+++ Reg4Event/bot.py
@@ -12,6 +12,11 @@ def start(update: Update, context: CallbackContext) -> None:
     message = "Доступные мероприятия:\n"
     for event_name, event_date, event_time in events:
         formatted_date = event_date.strftime("%d.%m.%Y")
+        
+        # Проверяем, является ли event_time строкой (в случае строки преобразуем в объект time)
+        if isinstance(event_time, str):
+            event_time = datetime.strptime(event_time, "%H:%M").time()
+        
         formatted_time = event_time.strftime("%H:%M")
         message += f"{event_name}: {formatted_date} {formatted_time}\n"
     update.message.reply_text(message)
@@ -24,10 +29,6 @@ def main():
 
     updater.start_polling()
     updater.idle()
-
-if __name__ == '__main__':
-    main()
-
 
 if __name__ == '__main__':
     main()
