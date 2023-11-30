4dfb081f3918e4faa61bc42c5836b8f683d49ac5
Alex <kaf-2012@mail.ru>
2023-11-30 23:21:32 +0300

    modified database.py

--- Reg4Event/database.py
+++ Reg4Event/database.py
@@ -7,6 +7,6 @@ class Database:
     def get_events(self):
         wb = openpyxl.load_workbook(self.filename)
         sheet = wb.active
-        events = [(row[0], row[1]) for row in sheet.iter_rows(min_row=2, values_only=True) if row[0] and row[1]]
+        events = [(row[0], row[1], row[2]) for row in sheet.iter_rows(min_row=2, values_only=True) if row[0] and row[1] and row[2]]
         wb.close()
         return events
