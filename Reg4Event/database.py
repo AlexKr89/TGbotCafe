2311e069769d7e092e7306a1c1d4a2f6db092eba
Alex <kaf-2012@mail.ru>
2023-11-30 23:58:36 +0300

    modified database.py

--- Reg4Event/database.py
+++ Reg4Event/database.py
@@ -27,4 +27,5 @@ class Database:
         cursor.execute('SELECT DISTINCT event_name, event_date, event_time FROM registrations')
         events = cursor.fetchall()
         conn.close()
+        print("Events from database:", events)
         return events
