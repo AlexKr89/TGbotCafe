ac0957a61da7ef1d1690384bc24fe0d1261c43a7
Alex <kaf-2012@mail.ru>
2023-12-03 22:48:14 +0300

    modified

--- Reg4Event/database.py
+++ Reg4Event/database.py
@@ -16,16 +16,14 @@ class Database:
 class RegistrationDatabase:
     def __init__(self, filename):
         self.filename = filename
-        self.wb = openpyxl.load_workbook(self.filename)
 
     def save_registration(self, event_name, user_info, user_phone):
-        sheet = self.wb.active
+        wb = openpyxl.load_workbook(self.filename)
+        sheet = wb.active
 
         # Добавляем user_info, user_phone и текущую метку времени в строку
         registration_data = [user_info, user_phone, datetime.now()]
         sheet.append([event_name, *registration_data])
 
-        self.wb.save(self.filename)
-
-    def close(self):
-        self.wb.close()
+        wb.save(self.filename)
+        wb.close()
