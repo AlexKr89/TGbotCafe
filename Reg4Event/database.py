60b255321bd8163a3f7cfae16d6a3fc678eafdbb
Alex <kaf-2012@mail.ru>
2023-12-03 19:36:47 +0300

    modified

--- Reg4Event/database.py
+++ Reg4Event/database.py
@@ -1,3 +1,4 @@
+# database.py
 import openpyxl
 
 class Database:
@@ -10,3 +11,22 @@ class Database:
         events = [(row[0], row[1], row[2]) for row in sheet.iter_rows(min_row=2, values_only=True) if row[0] and row[1] and row[2]]
         wb.close()
         return events
+
+class RegistrationDatabase:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def save_registration(self, event_name, user_info):
+        wb = openpyxl.load_workbook(self.filename)
+        sheet = wb.active
+
+        # Find the row where the event_name matches
+        for row in sheet.iter_rows(min_row=2, max_col=1, max_row=sheet.max_row, values_only=True):
+            if row[0] == event_name:
+                # Append user_info and current timestamp to the row
+                registration_data = [user_info, datetime.now()]
+                sheet.append([event_name, *registration_data])
+                break
+
+        wb.save(self.filename)
+        wb.close()
